8525cd407fc5cfb71227d080e27d0cd710290a47
jack <otiscurtisdotcom@gmail.com>
2022-08-14 22:23:34 +0100

    Fix loading and changing channels

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,8 @@ interface Item {
 }
 
 const App = () => {
+  const [started, setStarted] = useState(false);
+  const [loadingBar, setLoadingBar] = useState(0);
   const [mouseDown, setMouseDown] = useState(false);
   const [tenDocs, setTenDocs] = useState<Docs[]>();
   const [vids, setVids] = useState<string[]>();
@@ -40,7 +42,7 @@ const App = () => {
     )
   }
 
-  const go = () => {
+  const getTen = () => {
     fetchData(`${process.env.PUBLIC_URL}/archive.json`)
     .then((data: Archive) => {
       const docs: Docs[] = [];
@@ -52,6 +54,29 @@ const App = () => {
     })
   }
 
+  const changeChannel = () => {
+    const video = document.querySelector('video')!;
+    video.remove();
+    
+    console.log(`number left: `, document.querySelectorAll('video').length);
+
+    // Get another batch if running low...
+    if (document.querySelectorAll('video').length === 2 || vids && vids.length < 2) {
+      console.log('grabbing more');
+      getTen();
+    }
+
+    const newVideo = document.querySelector('video');
+    if (newVideo) {
+      // DEBUGGING
+      if (newVideo.duration) {
+        newVideo.currentTime = Math.floor(Math.random() * newVideo.duration);
+      }
+      newVideo.style.display = 'block';
+      newVideo.play();
+    }
+  }
+
   useEffect(() => {
     if (tenDocs) {
       for (const doc of tenDocs) {
@@ -61,8 +86,6 @@ const App = () => {
             // (file.format === "MPEG4" || file.format === "h.264" || file.format === "HiRes MPEG4" || file.format === "512Kb MPEG4") &&
             (file.name.indexOf("mp4") > -1)
           );
-
-          console.log(mp4Vid);
 
           if (mp4Vid && mp4Vid.size < 400000000) {
             const vidPath = `https://${data.d1}${data.dir}/${mp4Vid.name}`
@@ -76,11 +99,20 @@ const App = () => {
   }, [tenDocs]);
 
   useEffect(() => {
-    console.log(loadedCount, vids?.length);
+    if (vids) {
+      setLoadingBar(loadedCount / vids?.length * 100);
+    }
+
+    // Play first video
     if (vids && loadedCount >= vids?.length - 1) {
       const video = document.querySelector('video')!;
+
+      if (video.duration) {
+        video.currentTime = Math.floor(Math.random() * video.duration);
+      }
       video.play();
       video.style.display = 'block';
+      setStarted(true);
     }
   }, [loadedCount]);
 
@@ -92,19 +124,37 @@ const App = () => {
       }}
       onMouseDown={() => setMouseDown(true)}
       onMouseUp={() => setMouseDown(false)}
-      onClick={() => go()}
+      onClick={() => {
+        if (!started) {
+          getTen();
+        } else {
+          changeChannel();
+        }
+      }}
     >
+      {!started &&
+        <div className="loadingbar">
+          <span style={{width: `${loadingBar}%`}}></span>
+        </div>
+      }
+
       <div className="vid">
-        <div className="vidbucket">{videoPlayers()}</div>
-        <button className={`${mouseDown ? 'pressed' : ''}`}>
+        <div
+          className="vidbucket"
+          style={started ? {
+            backgroundImage: `url("${process.env.PUBLIC_URL}/img/static.gif")`
+          } : {}}
+        >
+          {!started &&
+            <p>Click anywhere to change the channel</p>
+          }
+          {videoPlayers()}
+        </div>
+        <span className={`button ${mouseDown ? 'pressed' : ''}`}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15"><path d="M10.5 1.674V4a5 5 0 0 1-3 9 5 5 0 0 1-3-9V1.674a7 7 0 1 0 6 0z"/><path d="M8.5 7.003V.997A.996.996 0 0 0 7.5 0c-.553 0-1 .453-1 .997v6.006c0 .551.444.997 1 .997.553 0 1-.453 1-.997z"/></svg>
-        </button>
+        </span>
       </div>
 
-      <div id="info">
-        <div id="loadingbar"><span></span></div>
-        <p>Click anywhere to change the channel</p>
-      </div>
     </main>
   );
 }
